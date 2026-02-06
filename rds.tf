ac65c9908396f6d89245bde979c1a1f84d41c5c1
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-06 17:41:25 +0100

    add rds mysql

---
+++ rds.tf
@@ -0,0 +1,20 @@
+resource "aws_db_instance" "wordpress" {
+  identifier        = "wordpress-db"
+  engine            = "mysql"
+  engine_version    = "8.0"
+  instance_class    = "db.t3.micro"
+  allocated_storage = 10
+
+  db_name  = "wordpress"
+  username = "wpuser"
+  password = "wpPassword123!"
+
+  db_subnet_group_name = aws_db_subnet_group.db_subnet_group.name
+  vpc_security_group_ids = [
+    aws_security_group.rds_sg.id
+  ]
+
+  publicly_accessible = false
+  skip_final_snapshot = true
+  deletion_protection = false
+}
