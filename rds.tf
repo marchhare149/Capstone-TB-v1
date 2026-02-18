29ebb18241ae07f12585c27787ba1931244ddda0
marchhare149 <buithiphuongthao14091994@gmail.com>
2026-02-18 14:28:57 +0100

    update rds

--- rds.tf
+++ rds.tf
@@ -9,12 +9,12 @@ resource "aws_db_instance" "wordpress" {
   username = "wpuser"
   password = "wpPassword123!"
 
-  db_subnet_group_name = aws_db_subnet_group.db_subnet_group_v1.name
-  vpc_security_group_ids = [
-    aws_security_group.rds_sg.id
-  ]
+  db_subnet_group_name   = aws_db_subnet_group.db_subnet_group_v1.name
+  vpc_security_group_ids = [aws_security_group.rds_sg.id]
 
+  multi_az            = true
   publicly_accessible = false
+
   skip_final_snapshot = true
   deletion_protection = false
 }
